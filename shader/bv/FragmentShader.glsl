3b01d0e877e5e440ce0602b5d97791ecd64cb2a3
Elisa Buxbaum <elisa.buxbaum@web.de>
2018-04-16 15:07:17 +0200

    Gauss 5x5, 7x7 (inkl. horizontal und vertikal)

--- shader/bv/FragmentShader.glsl
+++ shader/bv/FragmentShader.glsl
@@ -58,6 +58,28 @@ uniform vec2 offsets3x3[9] = vec2[]
 		vec2( 1,  -1), vec2( 1,  0), vec2( 1, 1)
 	);
 
+uniform vec2 offsets5x5[25] = vec2[] 
+(
+		vec2(-2, -2), vec2(-2, -1), vec2(-2, 0), vec2(-2, 1), vec2(-2, 2),
+		vec2(-1, -2), vec2(-1, -1), vec2(-1, 0), vec2(-1, 1), vec2(-1, 2),
+		vec2(0, -2), vec2(0, -1), vec2(0, 0), vec2(0, 1), vec2(0, 2),
+		vec2(1, -2), vec2(1, -1), vec2(1, 0), vec2(1, 1), vec2(1, 2),
+		vec2(2, -2), vec2(2, -1), vec2(2, 0), vec2(2, 1), vec2(2, 2)
+);
+
+uniform vec2 offsets7x7[49] = vec2[]
+(
+vec2(-3, -3), vec2(-3, -2), vec2(-3, -1), vec2(-3, 0), vec2(-3, 1), vec2(-3, 2), vec2(-3, 3),
+vec2(-2, -3), vec2(-2, -2), vec2(-2, -1), vec2(-2, 0), vec2(-2, 1), vec2(-2, 2), vec2(-2, 3),
+vec2(-1, -3), vec2(-1, -2), vec2(-1, -1), vec2(-1, 0), vec2(-1, 1), vec2(-1, 2), vec2(-1, 3),
+vec2(0, -3), vec2(0, -2), vec2(0, -1), vec2(0, 0), vec2(0, 1), vec2(0, 2), vec2(0, 3),
+vec2(1, -3), vec2(1, -2), vec2(1, -1), vec2(1, 0), vec2(1, 1), vec2(1, 2), vec2(1, 3),
+vec2(2, -3), vec2(2, -2), vec2(2, -1), vec2(2, 0), vec2(2, 1), vec2(2, 2), vec2(2, 3),
+vec2(3, -3), vec2(3, -2), vec2(3, -1), vec2(3, 0), vec2(3, 1), vec2(3, 2), vec2(3, 3)
+);
+
+
+
 
 layout(location = 0) out vec4 out_color;
 // =============================================================================================================
@@ -204,25 +226,112 @@ layout (index = 6) subroutine (FragmentProgram) void gauss3x3()
 
 layout (index = 7) subroutine (FragmentProgram) void gauss5x5()
 {
-    out_color = texture(textures.tex, Input.uv);
+    // für die Formel für H siehe https://de.wikipedia.org/wiki/Gau%C3%9F-Filter
+
+	// Weil wir eine 5x5 Umgebung betrachten
+	int m = 5;
+	const float pi = 3.141592653589;
+	
+	// Varianz die in der GUI eingegeben werden kann, wieder standardmäßig auf 1 setzen und in der Rage 0.01 und 10 bewegen lassen
+	float var = clamp((parameter.paramA.y) + 1, 0.01, 10);
+
+	// Filterkern
+	float H[25] = float[](	
+							0.f, 0.f, 0.f, 0.f, 0.f,
+							0.f, 0.f, 0.f, 0.f, 0.f,
+							0.f, 0.f, 0.f, 0.f, 0.f,
+							0.f, 0.f, 0.f, 0.f, 0.f,
+							0.f, 0.f, 0.f, 0.f, 0.f);
+	// Summe der Filterkerne; besseres Ergebnis als bei der Verwendung von m^2 bei der Teilung der Summe
+	float sumH = 0;
+	
+	vec4 texel = vec4(0.f, 0.f, 0.f, 1.f);
+
+	for(int i = 0; i < 25; ++i) {
+		H[i] = exp(-((offsets5x5[i].x)*(offsets5x5[i].x) + (offsets5x5[i].y)*(offsets5x5[i].y)) / (2.f * var)) / (2.f * pi * var);
+		texel += H[i] * texture(textures.tex, Input.uv + offsets5x5[i]);
+		sumH += H[i];
+		}
+
+    out_color = texel / sumH;
 }
 
 
 layout (index = 8) subroutine (FragmentProgram) void gauss7x7()
 {
-    out_color = texture(textures.tex, Input.uv);
+    // für die Formel für H siehe https://de.wikipedia.org/wiki/Gau%C3%9F-Filter
+
+	// Weil wir eine 7x7 Umgebung betrachten
+	int m = 7;
+	const float pi = 3.141592653589;
+	
+	// Varianz die in der GUI eingegeben werden kann, wieder standardmäßig auf 1 setzen und in der Rage 0.01 und 10 bewegen lassen
+	float var = clamp((parameter.paramA.y) + 1, 0.01, 10);
+
+	// Filterkern
+	float H[49] = float[](	
+							0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f,
+							0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f,
+							0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f,
+							0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f,
+							0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f,
+							0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f,
+							0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f);
+	// Summe der Filterkerne; besseres Ergebnis als bei der Verwendung von m^2 bei der Teilung der Summe
+	float sumH = 0;
+	
+	vec4 texel = vec4(0.f, 0.f, 0.f, 1.f);
+
+	for(int i = 0; i < 49; ++i) {
+		H[i] = exp(-((offsets7x7[i].x)*(offsets7x7[i].x) + (offsets7x7[i].y)*(offsets7x7[i].y)) / (2.f * var)) / (2.f * pi * var);
+		texel += H[i] * texture(textures.tex, Input.uv + offsets7x7[i]);
+		sumH += H[i];
+		}
+
+    out_color = texel / sumH;
 }
 
 
 layout (index = 9) subroutine (FragmentProgram) void gauss7x7vertical()
 {
-    out_color = texture(textures.tex, Input.uv);
+    float H[7] = float[]( 1.f, 2.f, 3.f, 4.f, 3.f, 2.f, 1.f);
+
+	vec2 offsets7x1[7] = vec2[]
+(
+vec2(-3, 0), 
+vec2(-2, 0), 
+vec2(-1, 0), 
+ vec2(0, 0), 
+ vec2(1, 0), 
+ vec2(2, 0), 
+ vec2(3, 0) 
+);
+
+vec4 texel = vec4(0);
+
+ for(int i = 0; i < 7; ++i)
+        texel += H[i] * texture(textures.tex, Input.uv + offsets7x1[i]);
+
+    out_color = texel / 16.f;
+
 }
 
 
 layout (index = 10) subroutine (FragmentProgram) void gauss7x7horizontal()
 {
-    out_color = texture(textures.tex, Input.uv);
+     float H[7] = float[]( 1.f, 2.f, 3.f, 4.f, 3.f, 2.f, 1.f);
+
+	  vec2 offsets1x7[7] = vec2[]
+(
+vec2(0, -3), vec2(0, -2), vec2(0, -1), vec2(0, 0), vec2(0, 1), vec2(0, 2), vec2(0, 3)
+);
+vec4 texel = vec4(0);
+
+ for(int i = 0; i < 7; ++i)
+        texel += H[i] * texture(textures.tex, Input.uv + offsets1x7[i]);
+
+    out_color = texel / 16.f;
+
 }
 
 
